129ceec2e6223cd9a29421cae4e88b85c9924758
Justin <justin@jjohnson.io>
2025-01-21 13:54:23 -0500

    loading and playing sounds with Tone.js

--- week-1-class/sketch.js
+++ week-1-class/sketch.js
@@ -1,4 +1,6 @@
-const sound = new SimplePlayer("");
+// init sound and connect to audio output
+const sound = new SimplePlayer("sounds/blip.wav");
+sound.toDestination();
 
 function setup() {
   createCanvas(400, 400);
@@ -7,3 +9,20 @@ function setup() {
 function draw() {
   background(0);
 }
+
+// function mouseClicked() {
+//   loaded && sound.start();
+// }
+
+function keyTyped() {
+  if (key == "a") {
+    loaded && sound.start();
+  }
+}
+
+// set a flag to show if the file has loaded yet
+let loaded = false;
+Tone.loaded().then(() => {
+  loaded = true;
+  console.log("loaded");
+});
