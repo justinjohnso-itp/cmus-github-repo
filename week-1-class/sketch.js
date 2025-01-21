bc9d7c23164b4a115fdf83113bdedbd701fe4b95
Justin <justin@jjohnson.io>
2025-01-21 14:03:02 -0500

    add keyboard mapping for multiple sounds

--- week-1-class/sketch.js
+++ week-1-class/sketch.js
@@ -1,6 +1,15 @@
-// init sound and connect to audio output
-const sound = new SimplePlayer("sounds/blip.wav");
-sound.toDestination();
+// init sounds and connect to audio output
+const blip = new SimplePlayer("sounds/blip.wav").toDestination();
+const pink = new SimplePlayer("sounds/pink.wav").toDestination();
+const takerimba = new SimplePlayer("sounds/takerimba.wav").toDestination();
+const tears = new SimplePlayer("sounds/tears.wav").toDestination();
+
+// set a flag to show if the file has loaded yet
+let loaded = false;
+Tone.loaded().then(() => {
+  loaded = true;
+  console.log("loaded");
+});
 
 function setup() {
   createCanvas(400, 400);
@@ -10,19 +19,18 @@ function draw() {
   background(0);
 }
 
-// function mouseClicked() {
-//   loaded && sound.start();
-// }
-
+// map keys to sounds
 function keyTyped() {
-  if (key == "a") {
-    loaded && sound.start();
+  console.log(key);
+  if (loaded) {
+    if (key == "a") {
+      blip.start();
+    } else if (key == "s") {
+      pink.start();
+    } else if (key == "d") {
+      takerimba.start();
+    } else if (key == "f") {
+      tears.start();
+    }
   }
 }
-
-// set a flag to show if the file has loaded yet
-let loaded = false;
-Tone.loaded().then(() => {
-  loaded = true;
-  console.log("loaded");
-});
