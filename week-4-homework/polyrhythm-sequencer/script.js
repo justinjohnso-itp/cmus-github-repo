07a4af65c18a3edfbb723a114c7288b661985958
Justin <justin@jjohnson.io>
2025-02-25 13:51:55 -0500

    move references

--- week-4-homework/polyrhythm-sequencer/script.js
+++ week-4-homework/polyrhythm-sequencer/script.js
@@ -1,3 +1,10 @@
+// Sequencer exercises in p5.js
+// https://editor.p5js.org/luisa/sketches/POSaDocKr – making a drum pattern
+// https://editor.p5js.org/luisa/sketches/HBYHJ96e7 - making a drum machine
+// https://editor.p5js.org/luisa/sketches/D0eixoM6O - adding interactivity
+
+// Mouse event handling for grid interaction – https://github.com/zjun2/SortingComparison/blob/d1bc59a89fb1ee2aeef1874cc8fd083b7c56d49d/%E7%94%9F%E5%91%BD%E6%B8%B8%E6%88%8F.html
+
 const cellSize = 40;
 let grid4 = Array(4)
   .fill()
